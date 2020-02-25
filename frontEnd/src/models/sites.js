d0353cb748349a2acec128b4420c20831e531b28
Emas Iraj Dam <emasiraj@gmail.com>
2020-02-25 15:45:40 +0100

    fixed favicon location

--- frontEnd/src/models/sites.js
+++ frontEnd/src/models/sites.js
@@ -1,15 +1,15 @@
 var m = require("mithril");
 
 var sites = { 
-    siteData: {},
+    siteData: [],
     loadList: function(searchValue) { 
         return m.request({ 
             method: "GET",
             url: "https://api.schroeff.com/sites/search?query=" + encodeURIComponent(searchValue),
         })
         .then(function(result) { 
-            console.log(result);
-            sites.siteData = result[0];
+            sites.siteData = result;
+            console.log(sites.siteData);
         })
     },
 };
