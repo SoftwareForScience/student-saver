6d7dd055f5bf32c8e3d32a736542bfb635c9aa6c
Emas Iraj Dam <emasiraj@gmail.com>
2020-02-26 12:01:33 +0100

    good luck with merge conflicts LOL

--- frontEnd/src/views/layout.js
+++ frontEnd/src/views/layout.js
@@ -1,18 +1,26 @@
 var m = require("mithril")
 
 module.exports = {
-    view: function(vnode) {
-        return m("header.masthead mb-auto", [
-            m("div.inner", [
-                m("h3.masthead-brand", "Student Saver", [
+    view: function (vnode) {
+        return m("div.cover-container d-flex h-100 p-3 mx-auto flex-column", [
+            m("header.masthead mb-auto", [
+                m("div.inner", [
+                    m("h3.masthead-brand", "Student Saver"),
                     m("nav.nav nav-masthead justify-content-center", [
                         m("a.nav-link active", "Search"),
                         m("a.nav-link", "Add")
-                    ]
-                    )
+                    ])
                 ])
-            ],
-            m("section", vnode.children))
+            ]),
+            m("main", vnode.children),
+            m("footer.mastfoot mt-auto", [
+                m("div.inner", [
+                    m("p", "Copyright ", [
+                        m("a", {href:"https://github.com/SoftwareForScience/student-saver"}, "Student Saver")
+                    ], " 2020")
+                ])
+            ])
+
         ])
     }
 }
